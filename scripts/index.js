fa9617f6e43e196c00df32d2edc679c5648765bf
nicholasburgher <55260105+nicholasburgher@users.noreply.github.com>
2020-01-15 08:46:59 -0600

    Version 3

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
-function loadPictures(nm, format, ct) {
+function loadPictures(tnm, nm, format, ct) {
   var cardColumns = document.querySelector("#gallery div.card-columns");
   for (var i = 0; i < ct; i++) {
     var formattedNumber = ("0" + (i + 1)).slice(-2);
@@ -7,10 +7,21 @@ function loadPictures(nm, format, ct) {
     divElem.setAttribute("class", "card");
     var imgElem = document.createElement("img");
     imgElem.setAttribute("class", "card-img-top");
-    imgElem.src = "images/" + nm + formattedNumber + "." + format;
+    imgElem.src = "images/thumbs/" + tnm + formattedNumber + "." + format;
+    imgElem.setAttribute("onclick","loadModal('" + tnm + formattedNumber + "." + format + "','" + nm + formattedNumber + "." + format + "');");
+    imgElem.setAttribute("data-toggle","modal");
+    imgElem.setAttribute("data-target","#previewModal");
     divElem.appendChild(imgElem);
     cardColumns.appendChild(divElem);
   }
 }
 
-window.onload = loadPictures("gatj-","jpg",65);
+function loadModal(thumbName,fullName) {
+  document.getElementById("previewTitle").innerHTML = fullName;
+  document.getElementById("selectedImg").src = "images/thumbs/" + thumbName;
+  var modalDownloadLink = document.getElementById("modalDownloadLink");
+  modalDownloadLink.href = "images/full/" + fullName;
+  //modalDownloadLink.download = fullName;
+}
+
+window.onload = loadPictures("gatj_thumb-","gatj-","jpg",78);
